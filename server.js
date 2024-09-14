2b2b8bf8e91312a9f8a7333fb7da7d9c4133bc7e
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2024-09-14 10:33:36 +0300

    updates

---
+++ server.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+// module dependencies
+var express = require('express');
+
+// get port from environment and store in Express
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+// index page
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+// info page
+app.get('/info', function(req, res) {
+	res.render('pages/info');
+});
+
+// Get port from environment and store in Express
+var port = process.env.PORT || 3000;
+app.listen(port)
+console.log('Server is listening on port', port)
